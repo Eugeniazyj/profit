04f7d5d7fb9df680c34e3ebaf4f7150882d8f3fa
Eugeniazyj <zhangyujieyx@hotmail.com>
2021-01-22 00:46:45 +0100

    regression try 21/02/2021

--- regression code.R
+++ regression code.R
@@ -1,19 +1,3 @@
-#regression
-library(plm)
-panel_final$turnover <- as.numeric(panel_final$turnover) #Warning message:NAs introduced by coercion 
-panelregression <- plm(formula = log(turnover) ~ THI_DAYS + precipitation + as.factor(year), data = panel_final, index = c("company", "year"), model = "within")#plm:panel lm(OLS)
-#should I use log here?
-#what about the NA values?
-#two seperated regression for turnover and ROE or together?
-# answer: seperately
-summary(panelregression)
-coeftest(panelregression, vcov = vcovDC(panelregression))
-
-# from Maarten, he run the same model again for subsamples based on the firm's employee size class,should I do the same?
-
-
-
-
 # next steps
 # 1. read through the documentation of fixest
 # https://www.rdocumentation.org/packages/fixest/versions/0.8.0
@@ -27,3 +11,19 @@ coeftest(panelregression, vcov = vcovDC(panelregression))
 # use summary(regression_output) to get it
 # for rainfall you need a marginal effects plot
 # see here https://www.patrickbaylis.com/blog/2018-04-12-svycontrasts/
+
+library(fixest)
+library(sjPlot)
+library(ggplot2)
+panel_final$turnover <- as.numeric(panel_final$turnover)
+panelregression1<- feols(turnover ~ THI_DAYS + precipitation + precipitation^2 | year+company, panel_final)#NOTE: 1,867 observations removed because of NA values (LHS: 56, RHS: 1,811).
+summary(panelregression1, se="twoway")
+# or: summary(panelregression1, cluster = ~year+company)
+# or: summary(panelregression1, cluster = panelregression[,c("year","company")])
+# I don't fully understand cluster here, when clustered only by company, precipitation becomes more significant
+panel_final$ROE <- as.numeric(gsub(",",".",panel_final$ROE)) # change "," to "." in order to make ROE numeric
+panelregression2 <- feols(ROE ~ THI_DAYS + precipitation + precipitation^2 | year+company, panel_final)#NOTE: 2,403 observations removed because of NA values (LHS: 735, RHS: 1,811).
+summary(panelregression2, se="twoway")
+plot_model(panelregression1,type = "pred",terms = "precipitation[all]")
+plot_model(panelregression2,type = "pred",terms = "precipitation[all]")
+# How to interpret marginal effects for precipitation.
