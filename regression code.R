068fd6280308e8db7ac6e8854d136caed4f40d6d
Eugeniazyj <zhangyujieyx@hotmail.com>
2021-01-26 15:52:49 +0100

    meeting 26/01/2021

--- regression code.R
+++ regression code.R
@@ -15,28 +15,36 @@
 library(fixest)
 library(sjPlot)
 library(ggplot2)
-panel_final$turnover <- as.numeric(panel_final$turnover)
-panelregressionTO<- feols(turnover ~ THI_DAYS + precipitation + precipitation^2 | year+company, panel_final)#NOTE: 1,867 observations removed because of NA values (LHS: 56, RHS: 1,811).
-summary(panelregressionTO, se="twoway")
-# or: summary(panelregression1, cluster = ~year+company)
-# or: summary(panelregression1, cluster = panelregression[,c("year","company")])
-# I don't fully understand cluster here, when clustered only by company, precipitation becomes more significant
+library(dplyr)
+#threshold=75
+panel_final$turnover <- as.numeric(panel_final$turnover) #change the type of turnover from character to numeric
+panelregressionTO<- feols(log(turnover+70) ~ THI_DAYS + precipitation + precipitation^2 | year+company, cluster = c("year","company") , panel_final)#do the fixed effects regression
+#NOTE: 1,867 observations removed because of NA values (LHS: 56, RHS: 1,811).
+summary(panelregressionTO)
+hist((panel_final$turnover+70))
+summary(panelregressionTO, se="twoway") #show the regression output
+# or: summary(panelregressionTO, cluster = ~year+company)
+# or: summary(panelregression, cluster = panelregression[,c("year","company")])
+### Question: I don't fully understand cluster here
 # when the threshold is 75, THI_DAYS is not significant; but when threhold changes to 80, all variables are significant at the 10% level
+summary(panel_final$ROE)
 panel_final$ROE <- as.numeric(gsub(",",".",panel_final$ROE)) # change "," to "." in order to make ROE numeric
-panelregressionROE <- feols(ROE ~ THI_DAYS + precipitation + precipitation^2 | year+company, panel_final)#NOTE: 2,403 observations removed because of NA values (LHS: 735, RHS: 1,811).
+panelregressionROE <- feols(ROE ~ THI_DAYS + precipitation + precipitation^2 | year+company, cluster = c("year","company"), panel_final)#NOTE: 2,403 observations removed because of NA values (LHS: 735, RHS: 1,811).
 summary(panelregressionROE, se="twoway")
 # whatever 75 or 80, not significant
-plot_model(panelregression1,type = "pred",terms = "precipitation[all]")
-plot_model(panelregression2,type = "pred",terms = "precipitation[all]")
-# How to interpret marginal effects for precipitation.
+plot_model(panelregressionTO,type = "pred",terms = "precipitation[all]")
+plot_model(panelregressionROE,type = "pred",terms = "precipitation[all]")
+### Question: How to interpret marginal effects for precipitation properly?
 
 
 #robust check
+###Question: I also do not fully understand robust check, I only have an abstract understanding. 
+###How to tell whether it is robust or not? 
 panelrobust <- panel_final
 panelrobust$turnover <- as.numeric(panelrobust$turnover)
 panelrobust$ROE <- as.numeric(gsub(",",".",panelrobust$ROE)) 
 summary(panelrobust$long)
-paneleast <- subset(panelrobust, panelrobust$long > 14.2)
+paneleast <- subset(panelrobust, panelrobust$long > 14.2) # use the same standard as Maarten
 panelwest <- subset(panelrobust, panelrobust$long < 14.2)
 summary(panelrobust$lat)
 panelnorth <- subset(panelrobust,panelrobust$lat > 46.75)
@@ -63,7 +71,6 @@ summary(panelregressionROEsouth,se="twoway")
 
 #Should I do like this?
 #Run the same model again for subsamples based on the firm's employee size class
-library(dplyr)
 panel_final$Number.of.employees.Last.avail..yr <- as.numeric(panel_final$Number.of.employees.Last.avail..yr)
 panel_final$companysize <-if_else(panel_final$Number.of.employees.Last.avail..yr <= 9, 1, if_else(panel_final$Number.of.employees.Last.avail..yr <= 50, 2, if_else(panel_final$Number.of.employees.Last.avail..yr<= 250, 3, 4)))
 panelsize1 <- subset(panel_final, panel_final$companysize == 1)
@@ -87,4 +94,4 @@ panel3regressionROE<- feols(ROE ~ THI_DAYS + precipitation + precipitation^2 | y
 summary(panel3regressionROE,se="twoway")
 panel4regressionROE<- feols(ROE ~ THI_DAYS + precipitation + precipitation^2 | year+company, panelsize4)
 summary(panel4regressionROE,se="twoway")
-#Question:How to make graghs for these results?
+###Question:How to make graphs for these results?
